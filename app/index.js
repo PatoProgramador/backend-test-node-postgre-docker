17efe95874ba105e48f2d0f2c389f57584b4a304
PatoProgramador <spartan1171947@hotmail.com>
2023-04-13 23:42:07 -0500

    projec organization/ basic server cors and middlewares

---
+++ app/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const PORT = process.env.EXTERNAL_PORT || 3001;
+
+const app = express();
+
+// setting cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+    next();
+});
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+
+
+
+
+try {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+} catch (error) {
+    console.log(error);
+}
