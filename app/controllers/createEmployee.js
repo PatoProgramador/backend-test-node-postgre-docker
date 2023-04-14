bb3b488f82b813d857f2e4e6f8680552f721cf62
PatoProgramador <spartan1171947@hotmail.com>
2023-04-14 11:46:54 -0500

    Basic post employee

--- app/controllers/createEmployee.js
+++ app/controllers/createEmployee.js
@@ -1,5 +1,15 @@
-const createEmployee = (req, res) => {
-    res.send('ola')
+const Empleado = require('../models/empleado');
+
+const createEmployee = async (req, res) => {
+    const { nif, nombre, apellido1, apellido2 } = req.body;
+    const newEmployee = await Empleado.create({
+        nif,
+        nombre,
+        apellido1,
+        apellido2
+    });
+
+    console.log(newEmployee);
 };
 
 module.exports = {createEmployee};
