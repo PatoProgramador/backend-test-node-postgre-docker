4cba8621ead73c32e47ae9ae9250e9720a3b68cc
PatoProgramador <spartan1171947@hotmail.com>
2023-04-14 14:09:43 -0500

    fix

--- app/controllers/employee/getEmployees.js
+++ app/controllers/employee/getEmployees.js
@@ -1,8 +1,18 @@
 const Empleado = require('../../models/empleado');
+const {Op} = require("sequelize");
 
 const getEmployees = async (req, res) => {
     try {
-        const employees = await Empleado.findAll();
+        let employees;
+        if(req.query) {
+            const {name} = req.query;
+            employees = await Empleado.findAll({
+                where: {
+                    nombre: {[Op.iLike]: `%${name}%`},
+                }
+            });
+        }
+        employees = await Empleado.findAll();
         res.status(200).json(employees) 
     } catch (error) {
         res.status(500).json({"error": error.message});
